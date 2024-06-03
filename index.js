b06e2ac8c6f5d51ded83459342fdd2fb4ee59ed1
A G <Anubh896@gmail.com>
2024-06-03 13:41:07 +0530

    initial-commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/routes.js';
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', router);
 
