ce5518e479e0bfbf50a6b9b2188630b6f904c739
bigbrandbucket-tech <162419472+bigbrandbucket-tech@users.noreply.github.com>
2024-09-05 19:14:17 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,15 @@ import Stripe from "stripe";
 const stripe = new Stripe(
   "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"
 );
+import paypal from '@paypal/checkout-server-sdk';
 
+// const environment = new paypal.core.LiveEnvironment(
+//   'AXR-yGRo04puXuV_nMIgGO6oeO_7PBcFxU0ieFLRei8oTHlvF1kyzhH5dTH6M7N1UV7PLJ03QzXPAzk6',
+//   'PAYPAL_CLIENT_SECRET'
+// );
+// const paypalClient = new paypal.core.PayPalHttpClient(environment);
+const environment = new paypal.core.SandboxEnvironment('AQbGfUMi_-GeuUpGOtoWVsC61MjF_b0zll4KaKHkOXjVadggTeVxhlFYdhe3ebRCr-lBrS_raHq9K01c', 'EIVscdKgNKWAXgIQY3Lfs1Q2ZUX38FnfFu9aVSqA5lQCgfPCirkxRdjcLymvUh5awjECFy8VBY_F0chV');
+const paypalClient = new paypal.core.PayPalHttpClient(environment);
 export const insertData = (req, res) => {
   const data = req.body;
   UserData.insert(data, (error, result) => {
@@ -65,19 +73,43 @@ export const getAllData = (req, res) => {
     res.json(result);
   });
 };
-export async function paymemtIntent(req, res) {
+export async function captureOrder (req, res) {
+  const { orderId } = req.body;
+  const request = new paypal.orders.OrdersCaptureRequest(orderId);
+  request.requestBody({});
+
   try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount:100, // Amount in cents
-      currency: 'usd',
+    const capture = await client.execute(request);
+    res.json({ status: capture.result.status, id: capture.result.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error capturing order');
+  }
+}
+export async function paymentIntent(req, res) {
+  try {
+    const request = new paypal.orders.OrdersCreateRequest();
+    request.prefer("return=representation");
+    request.requestBody({
+      intent: 'CAPTURE',
+      purchase_units: [{
+        amount: {
+          currency_code: 'USD',
+          value: '1000.00',  // Amount in USD
+        },
+      }],
     });
-    UserData.updateTransaction(req.body.id, paymentIntent.id, (error, result) => {
+
+    const order = await paypalClient.execute(request);
+    
+    UserData.updateTransaction(req.body.id, order.result.id, (error, result) => {
       if (error) {
-        console.error('Error updating data:', error);
+        console.error('Error updating transaction:', error);
         return res.status(500).json({ error: error.message });
       }
-    })
-    res.json({ client_secret: paymentIntent.client_secret });
+    });
+    
+    res.status(201).json({ id: order.result.id, status: order.result.status });
   
   } catch (err) {
     res.status(500).json({ error: err.message });
