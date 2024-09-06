2ef7d6f97a67aa72a638e36c37aa7b5782281ea3
bigbrandbucket-tech <162419472+bigbrandbucket-tech@users.noreply.github.com>
2024-09-06 14:36:45 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,41 @@
+// import express from "express";
+// import mysql from "mysql";
+
+
+// const app = express()
+// const port = 3000;
+
+// let con = mysql.createConnection({
+//     host: "srv1326.hstgr.io",
+//     port: 3306,
+//     user: "u619479099_crm",
+//     password: "Crm@2020#",
+//     database: "u619479099_crm",
+//     waitForConnections: true,
+//     connectionLimit: 5000,
+//     queueLimit: 5000,
+//   });
+
+
+// export default con;
 import express from "express";
 import mysql from "mysql";
 
-
-const app = express()
+const app = express();
 const port = 3000;
 
-let con = mysql.createConnection({
-    host: "srv1326.hstgr.io",
-    port: 3306,
-    user: "u619479099_crm",
-    password: "Crm@2020#",
-    database: "u619479099_crm",
-    waitForConnections: true,
-    connectionLimit: 5000,
-    queueLimit: 5000,
-  });
+// Create a connection pool
+let con = mysql.createPool({
+  host: "srv1326.hstgr.io",
+  port: 3306,
+  user: "u619479099_crm",
+  password: "Crm@2020#",
+  database: "u619479099_crm",
+  waitForConnections: true,
+  connectionLimit: 5000,  // Limit number of concurrent connections (adjust as needed)
+  queueLimit: 0,  // No limit on queued connection requests
+});
 
-
+// Export the pool
 export default con;
+
